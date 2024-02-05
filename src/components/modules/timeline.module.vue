9078071a8173d5fd5a38c3fe06f7e30f3fe670f0
ureniji <ureniji@icloud.com>
2024-02-05 17:45:50 +0100

    details timeline

--- src/components/modules/timeline.module.vue
+++ src/components/modules/timeline.module.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import { useInview } from '../../plugins/inview';
 import type Textblock from '../../interfaces/textblock.interface';
 import type Picture from '../../interfaces/picture.interface';
-import type Video from '../../interfaces/video.interface';
+import type Video from '../../interfaces/video.interface'; 
+import dayjs from 'dayjs';
 
 const props = defineProps<{
     picture?: Picture;
@@ -16,7 +17,21 @@ const props = defineProps<{
 
 const element = ref<HTMLElement>();
 const { inviewState } = useInview(element, {align: props.variant?.includes('align')});
-
+const formattedYear = computed(() =>  {
+    
+    if(props.date) {
+         const currentYear = dayjs(props.date).year();
+         return currentYear
+    }
+});
+const formattedDayMonth = computed(() =>  {
+    if(props.date) {
+        const dayMonth = dayjs(props.date).locale('de').format('DD, MMMM').toUpperCase();
+        dayjs(dayMonth);
+        console.log('dayMonth', dayMonth)
+        return dayMonth
+    }
+})
 </script>
 <template>
     <section  
@@ -26,16 +41,29 @@ const { inviewState } = useInview(element, {align: props.variant?.includes('alig
     :class="[inviewState, variant, {hasImage: picture, hasVideo: video}]">
    
     <!-- hier habe ich die Kontrolle über den Inhalt -->
-    <section class="content-container">
-        <time v-if="date">{{ date }}</time>
-        <!-- wenn Bild vorhanden, anzeigen -->
-        <lila-picture-partial v-if="picture" v-bind="picture"></lila-picture-partial>
+    <section class="elements-container">
+        <div class="date-container">
+            <time v-if="date" class="year" >
+                {{ formattedYear }} 
+        
+            </time>
+            <br /> 
+            <time v-if="date" class="dayMonth" >
+        
+                {{ formattedDayMonth }}
+            </time>
+        </div>
 
+        
+        <!-- wenn Bild vorhanden, anzeigen -->
+        <div class="media-container">
+            <lila-picture-partial v-if="picture" v-bind="picture"></lila-picture-partial>
         <!-- Wenn Video vorhanden, anzeigen -->
-        <lila-video-partial v-if="video" v-bind="video"></lila-video-partial>
+            <lila-video-partial v-if="video" v-bind="video"></lila-video-partial>
+        </div>
 
         <!--  hier drüber wird der Inhalt aller Elemente angepasst -->
-            <section class="elements-container">
+            <section class="text-container">
                 
                 <lila-textblock-partial v-bind="textblock" />
                 <lila-list-partial v-if="list" v-bind="list" class="list-container"></lila-list-partial>
@@ -44,17 +72,45 @@ const { inviewState } = useInview(element, {align: props.variant?.includes('alig
 
     </section>
 </template>
-<style scoped lang="less">
-.lila-module.timeline-module 
-.content-container {
-     
-        
-        border: dotted 2px red
-    
+<style lang="less" scoped>
+.lila-content-module .container .lila-module:first-child {
+    margin-top: 0;
 }
+.lila-module.timeline-module {
+     
+    margin: 0 auto;
+    width: @moduleWidth_L;
+    // height: calc(575px, 80px);
+}
+ .elements-container {
+    display: grid;
+    row-gap: 50px;
+    //  height: 575px; 
+     grid-template-columns: repeat(2, auto);
+     grid-template-rows:  repeat(3, auto);
+    //  position: relative;
+    
+     grid-template-areas: 
+     "date-container date-container"
+     "media-container text-container"
+     "media-container text-container";
+ }
 
-.elements-container  {
- background-color: yellow;
+&.date-container {
+    grid-area: date-container;
+    border: dotted 2px red;
+    width: auto;
+    
+    }
+
+&.media-container{
+    grid-area: media-container;
+    padding: 0 40px
+}
+&.text-cotainer {
+    padding: 0 40px;
+    border-left: solid 8px black; 
+    grid-area: text-container;
 }
 </style>
 
