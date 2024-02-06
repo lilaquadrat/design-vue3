307ca345f7532d334eb72f4dcd5f4a3d07635c4f
mschuebel <m.schuebel@lila2.de>
2024-02-06 14:46:20 +0100

    error partial

--- src/components/partials/error.partial.vue
+++ src/components/partials/error.partial.vue
@@ -1,10 +1,50 @@
 <script setup lang="ts">
+import useMainStore from '@/stores/main.store';
+import type { BasicData, Content } from '@lilaquadrat/interfaces';
+import type { SDKResponse } from '@lilaquadrat/sdk';
+import { prepareContent } from '@lilaquadrat/studio/lib/esm/frontend';
+import { watch } from 'vue';
+import { onBeforeMount } from 'vue';
+import { ref, computed } from 'vue';
 
-defineProps<{
+const props = defineProps<{
     status: number
+    type?: 'member' | 'public' | 'restricted'
+    hint?: string
 }>();
+const store = useMainStore();
+const data = ref<SDKResponse<BasicData<Content>>>();
+
+watch(props, () => getContent());
+
+function getContentId () {
+
+  if(props.hint) return props.hint;
+
+  const nameArray: string[] = ['error', props.status.toString()];
+
+  if(props.type) nameArray.push(props.type);
+
+  return nameArray.join('-');
+
+}
+
+async function getContent () {
+
+  data.value = await store.getContent({id: getContentId()});
+
+}
+
+onBeforeMount(async () => await getContent());
+
+const contentMerged = computed(() => {
+
+  if(data.value) return prepareContent(data.value?.data);
+  return prepareContent({})
+
+}); 
 
 </script>
 <template>
-    thats some error {{ status }}
+    <lila-content-module v-if="contentMerged" :content="contentMerged" />
 </template>
--- src/views/content.view.vue
+++ src/views/content.view.vue
@@ -17,7 +17,7 @@ const error = ref<boolean>(false);
 watch(route, () => getContent());
 onBeforeMount(() => getContent());
 
-store.setConfiguration({preloadImages: true})
+const contentType = computed(() => route.meta.contentType as 'public' | 'member');
 
 function getContentId (contentType: 'public' | 'member', pathMatch?: string) {
 
@@ -31,15 +31,13 @@ async function getContent () {
 
   loading.value = 0;
 
-  const contentType = route.meta.contentType as 'public' | 'member';
-
-  console.log(route.params, route.name, getContentId(contentType, route.params.pathMatch as string));
-
-  const contentId = getContentId(contentType, route.params.pathMatch as string);
+  const contentId = getContentId(contentType.value, route.params.pathMatch as string);
 
   data.value = await store.getContent({id: contentId});
 
   if(data.value.status === 200) {
+
+    error.value = false;
 
     if(data.value.data.settings.useLayout) {
 
@@ -56,6 +54,12 @@ async function getContent () {
   } else if (data.value.status === 403 || data.value.status === 401) {
 
     error.value = true;
+    loading.value = data.value.status;
+
+  } else if (data.value.status === 204 || data.value.status === 404) {
+
+    error.value = true;
+    loading.value = 404;
 
   } 
 
@@ -72,8 +76,7 @@ const contentMerged = computed(() => {
 
 <template>
     <article class="content-screen screen">
-      {{ error }}
-        <lila-error-partial v-if="error" />
+        <lila-error-partial v-if="error" :status="loading" :hint="loading === 404 ? 'error404' : undefined" :type="contentType" />
         <lila-content-module v-if="contentMerged" :content="contentMerged" />
     </article>
 </template>
