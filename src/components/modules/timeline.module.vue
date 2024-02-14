c0424842a8101f69fa2851d5b840387f9bbccbd5
mschuebel <m.schuebel@lila2.de>
2024-02-14 08:21:55 +0100

    fixed timeline css

--- src/components/modules/timeline.module.vue
+++ src/components/modules/timeline.module.vue
@@ -1,221 +1,218 @@
 <script setup lang="ts">
-import { ref, computed, onBeforeMount, watch } from 'vue';
+import { ref, computed, onBeforeMount } from 'vue';
 import { useInview } from '../../plugins/inview';
-import type Link from '../../interfaces/link.interface';
-import type Textblock from '../../interfaces/textblock.interface';
-import type Video from '../../interfaces/video.interface'; 
-import type Picture from '../../interfaces/picture.interface'; 
 import dayjs from 'dayjs';
-import type PicturegroupElement from '../../interfaces/PictureGroupElement.interface';
 import type ModuleBaseProps from '../../interfaces/ModuleBaseProps.interface';
-import type  { TimelineElement } from '../../interfaces/TimelineElement.interface';
-import { hardCopy } from '@lilaquadrat/studio/lib/esm/frontend';
+import type { TimelineElement } from '../../interfaces/TimelineElement.interface';
 
+defineOptions({ inheritAttrs: false });
 
-
-const props = defineProps<ModuleBaseProps &{
+const props = defineProps<
+  ModuleBaseProps & {
     elements: TimelineElement[];
     date: string;
-}>();
-
+  }
+>();
 const element = ref<HTMLElement>();
-const elementsExtended = ref<(TimelineElement & { position: 'left' | 'right'})[]>([]);
+const elementsExtended = ref<(TimelineElement & { position: 'left' | 'right' })[]>([]);
 
 // watch(() => props.elements, () => {
 
 //     console.log('watch:',setElements(elementsExtended))
- 
+
 // });
 
 onBeforeMount(() => {
-    console.log('onBeforeMount:', props.elements)
-    if(props.elements){
-        setElements(props.elements)
-    // console.log('onBeforeMount:', elementsExtended)
-    }
-})
-function setElements(elements: TimelineElement[]) {
-    const positionedItem: any[] = []
+  console.log('onBeforeMount:', props.elements);
 
-    elements.map((item, index: number) => {
-        let position: string;
-       if(index=== 0) {
-            position = 'left'
-       } else if (item.media.some((content)=> 
-            content.type === 'quote')) {
-            position = 'noMedia'
-       } else {
-        position = 'right' 
-       }
-        positionedItem.push({
-            ...item,
-            position
-        })
-    })
-    elementsExtended.value= positionedItem
+  if (props.elements) {
+    setElements(props.elements);
+    // console.log('onBeforeMount:', elementsExtended)
+  }
+});
+
+function setElements (elements: TimelineElement[]) {
+  const positionedItem: any[] = [];
+  let lastElementPosition: string;
+
+  elements.map((item, index: number) => {
+
+    let position: string = 'left';
+
+    if (index > 0 && item.media?.length) {
+
+      console.log(lastElementPosition);
+      if(lastElementPosition === 'left') position = 'right';
+
+    }
+
+    if(!item.media?.length) {
+
+      lastElementPosition = 'noMedia';
+    } else {
+
+      lastElementPosition = position;
+    }
+
+    positionedItem.push({
+      ...item,
+      position,
+    });
+
+  });
+
+  elementsExtended.value = positionedItem;
 }
 
-const { inviewState } = useInview(element, {align: props.variant?.includes('align')});
-
+const { inviewState } = useInview(element, { align: props.variant?.includes('align') });
 // kürzere Schreibweise des oberen Codes
 // wenn props.date existiert, dann bitte damit erstellen
 const formattedDate = computed(() => {
-    // null als spezielle Art
-  const date = props.date ? dayjs(props.date) : null; 
+  // null als spezielle Art
+  const date = props.date ? dayjs(props.date) : null;
+
   return {
-    year: date ? date.year() : '',
-    dayMonth: date ? date.locale('de').format('DD, MMMM').toUpperCase() : ''
+    year    : date ? date.year() : '',
+    dayMonth: date ? date.locale('de').format('DD, MMMM').toUpperCase() : '',
   };
 });
 
- const picturegroupItems = computed(() => {
-
- })
-
-
 </script>
 <template>
-    <section  
-        ref="element" 
-        :id="id"
-        class="lila-timeline-parent-container lila-module"
-        :class="[inviewState, variant]">
-            
-        <section class="elements-container">
-            <!-- Hier wird eine Liste von Elementen wie Bilder, Videos oder Textblöcke übergeben, damit mehrere Elemente untereinander gleichzeitig generiert werden können -->
-            <!-- Jedes Element erhält seine Position aus setElements -->
-            
-        <section class="singleElement-container" v-for="(element, index) in elementsExtended" :key="`timeline-withpositions${index}`" > 
-                <section class="time-container">
-                    <time v-if="date" class="year"> {{ formattedDate.year }}   </time>
-                    <time v-if="date" class="dayMonth" > {{ formattedDate.dayMonth }}</time> 
-                </section>
-                <section class="timeline-container"></section> 
-                <section class="media-container">
-                   <template v-if="element.media"> 
-                    <template v-for="(item, index) in element.media" :key="`picturepartial${index}`"> 
-                        <lila-picture-partial v-if="item.type === 'picture'" v-bind="item" />
-                        <lila-video-partial v-if="item.type === 'video'" v-bind="item"/> 
-                        <lila-quote-partial v-if="item.type === 'quote'" v-bind="item" /> 
-                    </template>
-                   </template>
-                </section>
-                <section class="text-container"> 
-                        <template v-if=element>
-                            <lila-textblock-partial  v-if="element.textblock" v-bind="element.textblock"/>
-                            <!-- <lila-quote-partial v-if="element.quote" v-bind="element.quote" /> {{ element }} -->
-                        </template>
-                </section>
+  <section ref="element" :id="id" class="lila-timeline-module lila-module" :class="[inviewState, variant]">
+    <section class="elements-container">
+      <!-- Hier wird eine Liste von Elementen wie Bilder, Videos oder Textblöcke übergeben, damit mehrere Elemente untereinander gleichzeitig generiert werden können -->
+      <!-- Jedes Element erhält seine Position aus setElements -->
+
+      <section class="singleElement-container" v-for="(element, index) in elementsExtended" :class="[element.position, {noMedia: !element.media}]" :key="`timeline-withpositions${index}`">
+        <section class="time-container">
+          <time v-if="date" class="year">{{ formattedDate.year }}</time>
+          <time v-if="date" class="dayMonth">{{ formattedDate.dayMonth }}</time>
         </section>
-        
-    </section>       
+        <section class="timeline-container"></section>
+        <section v-if="element.media" class="media-container">
+            <template v-for="(item, mediaIndex) in element.media" :key="`media-element-${mediaIndex}`">
+              <lila-picture-partial v-if="item.type === 'picture'" v-bind="item" />
+              <lila-video-partial v-if="item.type === 'video'" v-bind="item" />
+              <lila-quote-partial v-if="item.type === 'quote'" v-bind="item" />
+            </template>
+        </section>
+        <section v-if="element.textblock" class="text-container">
+            <lila-textblock-partial v-if="element.textblock" v-bind="element.textblock" />
+        </section>
+      </section>
     </section>
+  </section>
 </template>
 
 <style lang="less" scoped>
-.lila-timeline-parent-container {
-    .module; // zentriert alles 
-    max-width: @moduleWidth_L;
+.lila-timeline-module {
+  .module; // zentriert alles
+  max-width: @moduleWidth_L;
 
-    .time-container {
-        display: grid;
-        justify-self: end;
-        padding: 0 40px;
-        .font-head;
-        .year {
-            font-size: 60px;
-            line-height: 62px;
-            justify-self: end;
-            color: @color4;
-        }
-        .dayMonth {
-            font-size: @headline_L;
-            // line-height: @headline_XS;
-            color: @color1;
-            grid-row-start: 2;
-        }
-    }
-    .timeline-container {
-        grid-row-start: 2;
-        position: relative;
-
-        &::after,
-        &::before {
-            content: '';
-            position: absolute;
-            width: 8px;
-            background: @color4;
-            border-radius: 99px;
-            
-        }
-        &::after {
-            top: 0;
-            bottom: -80px;
-        }
-        &::before {
-            top: -250px;
-            bottom: 0;
-        }
-        @media @tablet {
-            width: 5px; 
-        }
-    }
-   
-    
-   .singleElement-container,
-   &.noMedia {
-  
+  .elements-container {
     display: grid;
-    grid-template-columns: 424px auto 696px;
-    // 424px minmax(auto, 1fr) minmax(auto, 1fr);
-    gap: 50px 0;
 
-        .media-container,
-        .picture-container {
-         display: flex;
-         flex-wrap: wrap; 
-         gap: 27px 0;  
-        grid-column-start: 1;
-        grid-row-start: 2;
-        padding: 0 40px
-        }
-        .text-container {
-        grid-row-start: 2;
-        padding: 0 40px
-        }    
-    
-    }
-    
-    &.right {
-        .singleElement-container {
-            .time-container {
-            grid-column-start: 3;
-            justify-self: start;
+    .singleElement-container {
+
+        display: grid;
+        grid-template-columns: 2fr 8px 4fr;
+        grid-template-rows: 75px max-content max-content 75px;
+        // 424px minmax(auto, 1fr) minmax(auto, 1fr);
+        gap: 40px;
+
+        .time-container {
+            display: grid;
+            justify-self: end;
+            justify-items: end;
+            .font-head;
+
+            grid-template-rows: max-content max-content;
+            gap: 5px;
+
+            grid-row-start: 2;
+            grid-row-end: 3;
+
             .year {
-                justify-self: start
+                font-size: 60px;
+                line-height: 62px;
+                color: @color4;
+            }
+            .dayMonth {
+                font-size: @headline_L;
+                color: @color1;
+                grid-row-start: 2;
             }
         }
-        
-        .media-container,
-        .picture-container {
+
+        .media-container {
+            display: grid;
+
+            gap: 40px 0;
+
+            grid-column-start: 1;
+
+            grid-row-start: 3;
+            grid-row-end: 4;
+
+            grid-auto-rows: max-content;
+
+            :deep(.lila-figure) {
+                grid-template-columns: auto;
+                justify-content: left;
+            }
+
+        }
+        .text-container {
+            grid-row-start: 3;
+            grid-row-end: 4;
+        }
+
+        .timeline-container {
+
+            grid-row-start: 1;
+            grid-row-end: 5;
+            grid-column-start: 2;
+
+            position: relative;
+
+            background-color: @color4;
+
+        }
+
+        &.noMedia {
+
+            grid-template-rows: 50px 1fr 50px;
+
+            .text-container {
+                grid-column-start: 3;
+                grid-row-start: 2;
+            }
+
+            .timeline-container {
+                grid-column-start: 2;
+                grid-row-start: 1;
+            }
+
+        }
+
+        &.right {
+            .text-container {
+                grid-column-start: 1;
+            }
+
+            .media-container, .time-container  {
                 grid-column-start: 3;
             }
-        }
-        .text-container {
-            grid-row-start: 2;
-            grid-column-start: 1;
-        } 
-        &.noMedia {
+
             .time-container {
-                grid-column-start: 1;
-                justify-self:end;
-                 .year  {
-                    justify-self:end;
-                 }
+                justify-self: start;
+                justify-items: start;
             }
-        }  
+        }
     }
+
+  }
+
 }
 </style>
-
-
